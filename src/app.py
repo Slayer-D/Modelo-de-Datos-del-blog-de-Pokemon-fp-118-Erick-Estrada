432d94cf4c351fe63757ddf160f772f8c4900fd0
Slayer-D <enriqueestrada741@gmail.com>
2025-09-30 00:59:27 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Item, Pokemon, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +44,101 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+
+#GET de usuarios y usuario
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    users = User.query.all()
+    print(users)
+    return jsonify([u.serialize() for u in users])
+
+@app.route("/users/<int:id>", methods=["GET"])
+def get_user(id):
+    user = User.query.get_or_404(id)
+    return jsonify(user.serialize())
+
+
+#GET de items e item
+
+@app.route("/items", methods=["GET"])
+def get_items():
+    items = Item.query.all()
+    print(items)
+    return jsonify([i.serialize() for i in items])
+
+@app.route("/items/<int:id>", methods=["GET"])
+def get_item(id):
+    item = Item.query.get_or_404(id)
+    return jsonify(item.serialize())
+
+#GET de pokemons y pokemon
+
+@app.route("/pokemons", methods=["GET"])
+def get_pokemons():
+    pokemons = Pokemon.query.all()
+    print(pokemons)
+    return jsonify([p.serialize() for p in pokemons])
+
+@app.route("/pokemons/<int:id>", methods=["GET"])
+def get_pokemon(id):
+    pokemon = Pokemon.query.get_or_404(id)
+    return jsonify(pokemon.serialize())
+
+
+
+#favorite
+
+@app.route("/favorites", methods=["GET"])
+def get_favorites():
+    favorites = Favorite.query.all()
+    return jsonify([f.serialize() for f in favorites])
+
+@app.route("/favorites/user/<int:user_id>", methods=["GET"])
+def get_favorites_by_user(user_id):
+    favorites = Favorite.query.filter_by(user_id=user_id).all()
+    return jsonify([f.serialize() for f in favorites])
+
+
+
+@app.route("/favorites", methods=["POST"])
+def create_favorite():
+    data = request.get_json()
+
+    user_id = data.get("user_id")
+    pokemon_id = data.get("pokemon_id")
+    item_id = data.get("item_id")
+
+    if not user_id:
+        return jsonify({"error": "Falta el user_id"}), 400
+
+    if not pokemon_id and not item_id:
+        return jsonify({"error": "Debes enviar al menos un pokemon_id o item_id"}), 400
+
+    favorite = Favorite(
+        user_id=user_id,
+        pokemon_id=pokemon_id,
+        item_id=item_id
+    )
+
+    db.session.add(favorite)
+    db.session.commit()
+
+    return jsonify(favorite.serialize()), 201
+
+
+@app.route("/favorites/<int:id>", methods=["DELETE"])
+def delete_favorite(id):
+    favorite = Favorite.query.get_or_404(id)
+
+    db.session.delete(favorite)
+    db.session.commit()
+
+    return jsonify({"message": "Favorito eliminado"}), 200
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
